243e1ab76a35184d69dccdcd3d9a239d087055ac
keerthanapk-22 <papaiyankeerthana@gmail.com>
2026-03-24 15:40:22 +0530

    file modifed

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,13 +2,6 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://www.amazon.in/');
 });
 
-test('nagivate instagram', async ({ page }) => {
-  await page.goto('https://www.instagram.com/?hl=en');
-  await expect(page).toHaveURL('https://www.instagram.com/?hl=en')
-  const title= await page.title()
-  console.log(title)
-
-});
